44d9031c88b96d9e43f07795080297d40a9d306c
MatthewShindel <matthew@shindel.com>
2024-03-03 18:07:59 -0500

    add sad path testing for cypress

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -65,6 +65,48 @@ describe('test happy path', () => {
 		.url().should('eq', 'http://localhost:3000/snoop')
 	})
 	it('Should be able to handle sad path errors, ie a bad city name or bad coordinates' , () => {
-		
+		cy.intercept({
+			method: 'GET',
+			url: 'https://open-weather13.p.rapidapi.com/city/yrok',
+			headers: {
+				'X-RapidAPI-Key': '8e3c03a57bmshcf9c926514fc323p15ca95jsna29d71f8a66a',
+				'X-RapidAPI-Host': 'open-weather13.p.rapidapi.com'
+			}
+		}, (req) => {
+			req.reply({
+				statusCode: 404,
+				fixture: 'badresponse.json'
+			});
+		});
+		cy.get('.locationForm input[name="location"]').type('yrok')
+		.get('.locationForm#byName .submitLocationButton').click()
+		.url().should('include', '/errorPage')
+		.get('.error h2').eq(0).should('contain.text', 'Oh No, you shouldn\'t be here!')
+		.get('.error h2').eq(1).should('contain.text', 'Perhaps a Bad Request?')
+		.get('.error a').click()
+		.url().should('include', '/snoop')
+
+		cy.intercept({
+			method: 'GET',
+			url: `https://open-weather13.p.rapidapi.com/city/latlon/51.5085/250`,
+			headers: {
+				'X-RapidAPI-Key': '8e3c03a57bmshcf9c926514fc323p15ca95jsna29d71f8a66a',
+				'X-RapidAPI-Host': 'open-weather13.p.rapidapi.com'
+			}
+		}, (req) => {
+			req.reply({
+				statusCode: 400,
+				fixture: 'badcoordinate.json'
+			});
+		});
+		cy.get('#byLatLong input[name="latitude"]').type('51.5085')
+    .get('#byLatLong input[name="longitude"]').type('250')
+		.get('#byLatLong button.submitLocationButton').click()
+		.url().should('include', '/errorPage')
+		.get('.error h2').eq(0).should('contain.text', 'Oh No, you shouldn\'t be here!')
+		.get('.error h2').eq(1).should('contain.text', 'Perhaps a Bad Request?')
+		.get('.error a').click()
+		.url().should('include', '/snoop')
+
 	})
 })
